40bdffa148a50adc63957eadd5e8fc39bf6f52e6
Otthon Leão <otthonleao@hotmail.com>
2024-11-16 00:51:38 -0400

    feat: add Transfer.java entity

---
+++ src/main/java/dev/otthon/transfermoney/entities/Transfer.java
@@ -0,0 +1,61 @@
+package dev.otthon.transfermoney.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_TRANSFER")
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "WALLET_SENDER_ID")
+    private Wallet sender;
+
+    @ManyToOne
+    @JoinColumn(name = "WALLET_RECEIVER_ID")
+    private Wallet receiver;
+
+    @Column(name = "VALUE")
+    private BigDecimal value;
+
+    public Transfer() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Wallet getSender() {
+        return sender;
+    }
+
+    public void setSender(Wallet sender) {
+        this.sender = sender;
+    }
+
+    public Wallet getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Wallet receiver) {
+        this.receiver = receiver;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
